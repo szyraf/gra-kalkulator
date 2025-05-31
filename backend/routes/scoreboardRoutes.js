f3bc576bcfc314b5383bb3072f19d4dd8247f281
sumpiotr <sumpiotr@gmail.com>
2025-05-31 19:09:16 +0200

    score database

--- backend/routes/scoreboardRoutes.js
+++ backend/routes/scoreboardRoutes.js
@@ -1,6 +1,59 @@
-const express = require('express');
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const bodyParser = require("body-parser");
 
 const router = express.Router();
 
-
 module.exports = router;
+
+app.use(bodyParser.json());
+
+//database connection
+const db = new sqlite3.Database("../scores.db", (err) => {
+    if (err) return console.error(err.message);
+    console.log("Połączono z bazą SQLite.");
+});
+
+//create db
+db.run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    score INTEGER NOT NULL
+  )
+`);
+
+router.post("/score", (req, res) => {
+    const { username, score } = req.body;
+    const query = `INSERT INTO users (username, number) VALUES (?, ?)`;
+
+    db.run(query, [username, score], function (err) {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.status(201).json({ id: this.lastID, username, score });
+    });
+});
+
+// Endpoint: pobierz wszystkich użytkowników
+app.get("/score/all", (req, res) => {
+    const query = `SELECT * FROM users`;
+
+    db.all(query, [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(rows);
+    });
+});
+
+app.get("/score/:count", (req, res) => {
+    const query = `SELECT * FROM users LIMIT (?)`;
+
+    db.all(query, [req.params.count], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(rows);
+    });
+});
