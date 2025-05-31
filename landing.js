1623e40d3b9168b0d8ef519aa190969caff283bf
Kenzocenzo <aleksrogoz@gmail.com>
2025-06-01 01:58:37 +0200

    tadek działa

--- landing.js
+++ landing.js
@@ -2,17 +2,16 @@ if (localStorage.getItem("username")) {
     document.getElementById("landing-page").style.display = "none";
 }
 
-function startGame() {
-    const el = document.getElementById("landing-page");
-    let username = document.getElementById("username").value.trim();
-    if (username != "") {
-        localStorage.setItem("username", username);
-        el.style.display = "none";
-    } else {
-        alert("Nazwa użytkownika nie może być pusta!");
-    }
-}
+function startGame(){
+  
+  const el = document.getElementById("landing-page");
+  let username = document.getElementById("username").value.trim()
+  if (username != "") {
+    localStorage.setItem("username", username);
+    el.style.display = "none";
+    showTadekDialog(0);
 
-function showTutorial() {
-    startGame();
+  }else{
+    alert("Nazwa użytkownika nie może być pusta!")
+  }
 }
