e9395bcdac900075b7801cd42bd9ac2655b5eadf
Filip <ploszajf@gmail.com>
2025-05-31 22:00:03 +0200

    możliwosc wylaczenia landing pagea XD

---
+++ landing.js
@@ -0,0 +1,10 @@
+function startGame(){
+      const el = document.getElementById("landing-page");
+  if (el) {
+    el.style.display = "none";
+  }
+}
+
+function showTutorial(){
+    startGame();
+}
