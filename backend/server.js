a08e44e1f8c9b35df2eb2d676861f8a2aa6d856d
Filip <ploszajf@gmail.com>
2025-05-31 18:38:43 +0200

    dziala

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,12 @@ const cors = require('cors');
 
 const scoreboardRoutes = require('./routes/scoreboardRoutes');
 
-dotenv.config();
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/scoreboard', scoreboardRoutes);
+//app.use('/api/scoreboard', scoreboardRoutes);
 
 const PORT =  5000;
 app.listen(PORT, () => {
