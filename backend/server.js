8f28feead92f8b5765003c19f6198a2848923b54
Filip <ploszajf@gmail.com>
2025-05-31 18:35:18 +0200

    backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+
+const scoreboardRoutes = require('./routes/scoreboardRoutes');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/scoreboard', scoreboardRoutes);
+
+const PORT =  5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Serwer działa na porcie ${PORT}`);
+});
