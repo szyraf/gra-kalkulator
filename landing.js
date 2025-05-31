4c48d9bf7f716bb9497c8e3a288805a0d9238935
Filip <ploszajf@gmail.com>
2025-05-31 22:15:02 +0200

    username do storage, display none landingpage

--- landing.js
+++ landing.js
@@ -1,7 +1,17 @@
+window.onload = () =>{
+  if(localStorage.getItem("username")){
+    document.getElementById("landing-page").style.display = "none";
+  }
+}
+
 function startGame(){
       const el = document.getElementById("landing-page");
-  if (el) {
+      let username = document.getElementById("username").value.trim()
+  if (username != "") {
+    localStorage.setItem("username", username);
     el.style.display = "none";
+  }else{
+    alert("Nazwa użytkownika nie może być pusta!")
   }
 }
 
