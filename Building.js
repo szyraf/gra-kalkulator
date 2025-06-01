b49fd6f3204cc581146a2a356b54d182df7eab15
sumpiotr <sumpiotr@gmail.com>
2025-06-01 04:10:16 +0200

    comments

--- Building.js
+++ Building.js
@@ -27,13 +27,13 @@ export class Building {
     let weather = JSON.parse(localStorage.getItem("weather"))[0];
     if (this.energyType == EnergyType.wind) {
       let speed = (weather.windSpeed * 1000) / 3600;
-      return this.#round(((0.5 * 1.225 * 3.14 * 5026.5 * speed * speed * speed * 0.45) / 1000) * 15);
+      return this.#round(((0.5 * 1.225 * 3.14 * 5026.5 * speed * speed * speed * 0.45) / 1000) * 15); //70m - promień
     } else if (this.energyType == EnergyType.solarSmall) {
       let sunPercent = 1 - weather.cloud / 100;
-      return this.#round(27 * 1 * sunPercent * 0.85);
+      return this.#round(27 * 1 * sunPercent * 0.85); //27kw - średnia moc
     } else if (this.energyType == EnergyType.solar) {
       let sunPercent = 1 - weather.cloud / 100;
-      return this.#round(86.82 * 1 * sunPercent * 0.85);
+      return this.#round(86820 * 1 * sunPercent * 0.85); //86820kw - średnia moc
     } else {
       return 0;
     }
