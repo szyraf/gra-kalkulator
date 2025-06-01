0df06eb45d392cc3deb5d80f2f78b253757e26ab
sumpiotr <sumpiotr@gmail.com>
2025-06-01 04:07:37 +0200

    energy calculations

--- Building.js
+++ Building.js
@@ -24,7 +24,23 @@ export class Building {
   }
 
   getProducedEnergy() {
-    return this.energyPerHour;
+    let weather = JSON.parse(localStorage.getItem("weather"))[0];
+    if (this.energyType == EnergyType.wind) {
+      let speed = (weather.windSpeed * 1000) / 3600;
+      return this.#round(((0.5 * 1.225 * 3.14 * 5026.5 * speed * speed * speed * 0.45) / 1000) * 15);
+    } else if (this.energyType == EnergyType.solarSmall) {
+      let sunPercent = 1 - weather.cloud / 100;
+      return this.#round(27 * 1 * sunPercent * 0.85);
+    } else if (this.energyType == EnergyType.solar) {
+      let sunPercent = 1 - weather.cloud / 100;
+      return this.#round(86.82 * 1 * sunPercent * 0.85);
+    } else {
+      return 0;
+    }
+  }
+
+  #round(number) {
+    return Math.round(number * 100) / 100;
   }
 }
 
@@ -36,6 +52,7 @@ export const BuildingType = {
 
 export const EnergyType = {
   solar: 1,
-  wind: 2,
-  none: 3,
+  solarSmall: 2,
+  wind: 3,
+  none: 4,
 };
