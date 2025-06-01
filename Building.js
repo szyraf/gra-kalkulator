088be7df9cfa585cf9b43e788ab5cef8e5f9e3e3
sumpiotr <sumpiotr@gmail.com>
2025-06-01 06:15:15 +0200

    energy

--- Building.js
+++ Building.js
@@ -19,20 +19,22 @@ export class Building {
     this.type = BuildingType[jsonObject["type"]];
     this.gridX = gridX;
     this.gridY = gridY;
-    this.energyType = EnergyType.wind;
+    this.energyType = EnergyType[jsonObject["energyType"]];
     this.currentEnergy = 0;
   }
 
-  getProducedEnergy() {
+  getProducedEnergy(hour) {
     let weather = JSON.parse(localStorage.getItem("weather"))[0];
     if (this.energyType == EnergyType.wind) {
       let speed = (weather.windSpeed * 1000) / 3600;
       return this.#round(((0.5 * 1.225 * 3.14 * 5026.5 * speed * speed * speed * 0.45) / 1000) * 15); //70m - promień
     } else if (this.energyType == EnergyType.solarSmall) {
-      let sunPercent = 1 - weather.cloud / 100;
+      if (hour > weather.sunset || hour < weather.sunrise) return 0;
+      let sunPercent = 1 - weather.cloudCoverage / 100;
       return this.#round(27 * 1 * sunPercent * 0.85); //27kw - średnia moc
     } else if (this.energyType == EnergyType.solar) {
-      let sunPercent = 1 - weather.cloud / 100;
+      if (hour > weather.sunset || hour < weather.sunrise) return 0;
+      let sunPercent = 1 - weather.cloudCoverage / 100;
       return this.#round(86820 * 1 * sunPercent * 0.85); //86820kw - średnia moc
     } else {
       return 0;
--- TurnManager.js
+++ TurnManager.js
@@ -1,40 +1,45 @@
+import { nextDay } from "./date-weather.js";
 const hourDuration = 2000;
 
 export class TurnManager {
-    #updateInterval;
-    #game;
-    #playing = false;
-    #hour = 8;
+  #updateInterval;
+  #game;
+  #playing = false;
+  #hour = 8;
 
-    constructor(game) {
-        this.#hour = 8;
-        this.#game = game;
-        document.getElementById("startTurnButton").addEventListener("click", () => {
-            this.playTurn(this);
-        });
-    }
+  constructor(game) {
+    this.#hour = 8;
+    this.#game = game;
+    document.getElementById("startTurnButton").addEventListener("click", () => {
+      this.playTurn(this);
+    });
+  }
 
-    playTurn() {
-        if (this.#playing) return;
-        this.#playing = true;
-        this.#hour = 8;
-        this.#updateInterval = window.setInterval(() => {
-            this.#update();
-        }, hourDuration);
-    }
+  playTurn() {
+    if (this.#playing) return;
+    this.#playing = true;
+    this.#hour = 8;
+    this.#game.updateTimeInfo(this.#hour);
+    this.#updateInterval = window.setInterval(() => {
+      this.#update();
+    }, hourDuration);
+  }
 
-    #update() {
-        if (this.#hour >= 32) {
-            window.clearInterval(this.#updateInterval);
-            this.#endTurn();
-            return;
-        }
-        this.#hour += 1;
-        this.#game.updateEnergy();
+  #update() {
+    if (this.#hour == 25) {
+      //nextDay();
+    } else if (this.#hour >= 32) {
+      window.clearInterval(this.#updateInterval);
+      this.#endTurn();
+      return;
     }
+    this.#hour += 1;
+    this.#game.updateEnergy(this.#hour % 24);
+    this.#game.updateWeatherInfo();
+    this.#game.updateTimeInfo(this.#hour % 24);
+  }
 
-    #endTurn() {
-        //nextDay();
-        this.#playing = false;
-    }
+  #endTurn() {
+    this.#playing = false;
+  }
 }
